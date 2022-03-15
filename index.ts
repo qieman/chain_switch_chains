ba16761098d03faa107e38c1cf060985aa759df7
ZOU <253058016@qq.com>
2022-03-15 14:51:32 +0800

    update icons

--- index.ts
+++ index.ts
@@ -2,19 +2,16 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 function minify (str: string) {
-    return str.replace(/[\n|\s]/g, '').replace(/\r\n/g, '')
+    return str.replace(/[\n]/g, '').replace(/\r\n/g, '')
 }
 
-function combine (fromPath: string, output: string) {
+function combineChain (fromPath: string, output: string) {
     const files = fs.readdirSync(fromPath)
     let res = ''
     files.forEach((item, index) => {
         const filePath = path.join(fromPath, item)
         try {
             let data = fs.readFileSync(filePath, 'utf-8')
-            if (index !== files.length) {
-                data
-            }
             res = index !== files.length - 1 ? res + data + ',' : res + data
         } catch (e) {
             console.log('fromPath', fromPath)
@@ -25,14 +22,36 @@ function combine (fromPath: string, output: string) {
 
     res = `[${res}]`
     fs.writeFileSync(path.join(output), res)
-    fs.writeFileSync(path.join(output.replace('.json', '.min.json')), minify(res))
+    //fs.writeFileSync(path.join(output.replace('.json', '.min.json')), minify(res))
 }
 
 
+function combineIcons(fromPath: string, output: string) {
+    const files = fs.readdirSync(fromPath)
+    let res = ''
+    files.forEach((item, index) => {
+        const filePath = path.join(fromPath, item)
+        try {
+            let data = fs.readFileSync(filePath, 'utf-8')
+            let dataJson = JSON.parse(data)
+            const key = item.replace('.json', '')
+            let string = `${key}: "${dataJson[0].url}"`
+            res = index !== files.length - 1 ? res + string + ',' : res + string
+        } catch (e) {
+            console.log('fromPath', fromPath)
+            console.log('item', item)
+            console.error(e)
+        }
+    })
+
+    res = `{${res}}`
+    fs.writeFileSync(path.join(output), res)
+}
 
 const chainsPath = path.join('src/chains')
 const iconsPath = path.join('src/icons')
-combine(chainsPath, 'dist/chains.json')
-combine(iconsPath, 'dist/icons.json')
+combineChain(chainsPath, 'dist/chains.json')
+combineIcons(iconsPath, 'dist/icons.json')
+
 
 
